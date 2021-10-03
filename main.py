ddfe4bfd46bdd0ef953421f943c8af6de54e3cab
avaloshka <81110315+avaloshka@users.noreply.github.com>
2021-10-03 17:11:26 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+# @app.route('/action_page.php')
+# def action_page():
+#     return render_template('action_page.php')
+
+
+@app.route('/login', methods=["POST"])
+def receive_data():
+    name = request.form["username"]
+    password = request.form["password"]
+    return f"<h1>Name: {name}, Password: {password}</h1"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
